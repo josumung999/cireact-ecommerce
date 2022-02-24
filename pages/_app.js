c2440b784ceb0770c8a7d949525a12f8e0b1ae2e
Josué <jmunganga039@gmail.com>
2022-02-24 15:49:47 +0200

    Intial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
 import '../styles/globals.css'
 
+// Importing bootstrap js files
+//import 'bootstrap/dist/js/bootstrap.bundle';
+
+
 function MyApp({ Component, pageProps }) {
+
   return <Component {...pageProps} />
 }
 
