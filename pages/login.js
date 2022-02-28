586bf8045021bddd8422ed179e87e54a01c00b22
Josué <jmunganga039@gmail.com>
2022-02-28 14:50:59 +0200

    Building Login and register components

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Login from '../components/auth/Login'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Login to your account</title>
+      </Head>
+      <Login />
+    </>
+  )
+}
---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Register from '../components/auth/Register'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Register and start buying from CI-react</title>
+      </Head>
+      <Register />  
+    </>
+  )
+}
